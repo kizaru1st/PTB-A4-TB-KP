edfdcf16aeb2ef4c52edcc2d6b9ab7ce2dfa900f
Reza Rizki <rezarizki77@gmail.com>
2022-11-03 16:53:11 +0700

    .

---
+++ app/src/main/java/com/example/latihanandroid/EditPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.latihanandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_password)
+    }
+}
