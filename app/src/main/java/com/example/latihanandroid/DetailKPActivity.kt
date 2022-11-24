0befd10042ff8ac8038387c5fe94045453d0e8df
Reza Rizki <rezarizki77@gmail.com>
2022-11-24 10:52:31 +0700

    .

---
+++ app/src/main/java/com/example/latihanandroid/DetailKPActivity.kt
@@ -0,0 +1,11 @@
+package com.example.latihanandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailKPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_kpactivity)
+    }
+}
