146c30d17b536a340a4dee04c2eedf6df16c89a7
Reza Rizki <rezarizki77@gmail.com>
2022-12-10 13:08:57 +0700

    .

--- app/src/main/java/com/example/latihanandroid/SignInActivity.kt
+++ app/src/main/java/com/example/latihanandroid/SignInActivity.kt
@@ -1,19 +1,54 @@
 package com.example.latihanandroid
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
+    val CHANNEL_ID_login = "channelID_login"
+    val CHANNEL_NAME_login = "channelName_login"
+    val NOTIFICATION_ID_login = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID_login)
+            .setContentTitle("PTB A4")
+            .setContentText("Berhasil Login!")
+            .setSmallIcon(R.drawable.ic_star)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+        val notificationManager = NotificationManagerCompat.from(this)
 
         btnSignIn.setOnClickListener {
+            notificationManager.notify(NOTIFICATION_ID_login, notification)
             var intent = Intent(this@SignInActivity, HomeActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID_login, CHANNEL_NAME_login, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
         }
     }
 }
