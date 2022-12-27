1e14c8ef596ba17b21c0db01c5d203fdddf8a7c3
Reza Rizki <rezarizki77@gmail.com>
2022-12-27 09:39:58 +0700

    .

--- app/src/main/java/com/example/latihanandroid/EditProfileActivity.kt
+++ app/src/main/java/com/example/latihanandroid/EditProfileActivity.kt
@@ -10,20 +10,37 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.example.latihanandroid.datamodels.LoginResponse
+import com.example.latihanandroid.datamodels.UpdateProfileResponse
+import com.example.latihanandroid.retrofit.Api
 import kotlinx.android.synthetic.main.activity_edit_profile.*
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class EditProfileActivity : AppCompatActivity() {
     val CHANNEL_ID_EditProfile = "channelID_EditProfile"
     val CHANNEL_NAME_EditProfile = "channelName_EditProfile"
     val NOTIFICATION_ID_EditProfile = 0
+    lateinit var editEmail: EditText
+    lateinit  var editNama: EditText
+    lateinit  var buttonSave: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel()
         }
@@ -42,6 +59,7 @@ class EditProfileActivity : AppCompatActivity() {
         val notificationManager = NotificationManagerCompat.from(this)
 
         btnReset.setOnClickListener {
+            saveProfile()
             notificationManager.notify(NOTIFICATION_ID_EditProfile, notification)
             var intent = Intent(this@EditProfileActivity, HomeActivity::class.java)
             startActivity(intent)
@@ -59,5 +77,46 @@ class EditProfileActivity : AppCompatActivity() {
             val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             manager.createNotificationChannel(channel)
         }
+    }
+
+    fun saveProfile() {
+        val sharedPref = applicationContext.getSharedPreferences("sharedPref", Context.MODE_PRIVATE) ?: return
+        val token = sharedPref.getString("TOKEN", "")
+
+        editEmail = findViewById(R.id.editEmailProfile)
+        editNama = findViewById(R.id.editNamaProfile)
+        buttonSave = findViewById(R.id.btnReset)
+        var nameProfile = editNama.getText().toString()
+        var emailProfile = editEmail.getText().toString()
+
+        val API_BASE_URL = "http://ptb-api.husnilkamil.my.id/api/"
+        val retrofit = Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient.Builder().build())
+            .build()
+
+        val client = retrofit.create(Api::class.java)
+        val call: Call<UpdateProfileResponse> = client.updateProfile(token = "Bearer $token", nameProfile, emailProfile)
+        call!!.enqueue(object : Callback<UpdateProfileResponse?> {
+            override fun onResponse(
+                call: Call<UpdateProfileResponse?>,
+                response: Response<UpdateProfileResponse?>
+            ) {
+                val respon: UpdateProfileResponse? = response.body()
+                if (respon != null && respon.status == "success" ) {
+                    Toast.makeText(this@EditProfileActivity, "Berhasil Mengupdate Profile", Toast.LENGTH_SHORT).show()
+                    intent = Intent(applicationContext, ProfileActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this@EditProfileActivity, "Salah", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<UpdateProfileResponse?>, t: Throwable) {
+                Toast.makeText(this@EditProfileActivity, t.message, Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/latihanandroid/retrofit/Api.kt
+++ app/src/main/java/com/example/latihanandroid/retrofit/Api.kt
@@ -3,6 +3,7 @@ package com.example.latihanandroid.retrofit
 import com.example.latihanandroid.datamodels.GetProfileResponse
 import com.example.latihanandroid.datamodels.LoginResponse
 import com.example.latihanandroid.datamodels.LogoutResponse
+import com.example.latihanandroid.datamodels.UpdateProfileResponse
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -13,8 +14,18 @@ interface Api {
         @Field("username") username: String,
         @Field("password") password: String
     ): Call<LoginResponse?>?
+
     @POST("logout")
     fun logout(@Header("Authorization") token: String) : Call<LogoutResponse>
+
     @GET("/api/me")
     fun profile(@Header("Authorization") token:String):Call<GetProfileResponse>
+
+    @FormUrlEncoded
+    @POST("/api/me/update")
+    fun updateProfile(
+        @Header("Authorization") token:String,
+        @Field("name") name:String,
+        @Field("email") email:String
+    ):Call<UpdateProfileResponse>
 }
