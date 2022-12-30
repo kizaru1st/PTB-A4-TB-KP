db1b2ab68cf1ac2ce5813f08e14bf86a3712ff25
Reza Rizki <rezarizki77@gmail.com>
2022-12-30 13:51:18 +0700

    .

--- app/src/main/java/com/example/latihanandroid/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/latihanandroid/MyFirebaseMessagingService.kt
@@ -2,9 +2,7 @@ package com.example.latihanandroid
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -13,55 +11,45 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+    private val channel_id = "contoh"
+    private val notifId = 1
 
     override fun onNewToken(token: String) {
-        val TAG = "Service-Debug"
-        Log.d(TAG, "Refreshed token: $token")
+        super.onNewToken(token)
     }
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        val a = message.notification?.title.toString()
+        val b = message.notification?.body.toString()
 
-        if(remoteMessage.notification != null){
-            createNotificationChannel()
+        createNotificationChannel()
+        showNotification(a, b)
+    }
+
+    fun showNotification (title: String, message: String){
+        val builder = NotificationCompat.Builder(this, channel_id)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(R.drawable.ic_star)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        with(NotificationManagerCompat.from(this)){
+            notify(1, builder.build())
         }
     }
 
-    private fun createNotificationChannel() {
-
-        val CHANNEL_ID = "Home"
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "PTBA4"
-            val descriptionText = "Firebase PTB A4"
-            val importance = NotificationManager.IMPORTANCE_HIGH
-
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification title"
+            val descriptionText = "Notification description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channel_id, name, importance).apply {
                 description = descriptionText
             }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.createNotificationChannel(channel)
         }
-
-        // Create an explicit intent for an Activity in your app
-        val intent = Intent(this, HomeActivity::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_star)
-            .setContentTitle("PTB A4")
-            .setContentText("Ini adalah Notification Manual Firebase")
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            // Set the intent that will fire when the user taps the notification
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
-
-        with(NotificationManagerCompat.from(this)) {
-            // notificationId is a unique int for each notification that you must define
-            notify(12312, builder.build())
-        }
     }
+
 }
+
