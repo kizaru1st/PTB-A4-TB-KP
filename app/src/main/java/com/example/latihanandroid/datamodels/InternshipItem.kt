071fe6b402c63ec55391d04a0602c639e98ce4b3
annisafaras <89978961+annisafaras@users.noreply.github.com>
2023-01-04 22:54:48 +0700

    2011521005

---
+++ app/src/main/java/com/example/latihanandroid/datamodels/InternshipItem.kt
@@ -0,0 +1,13 @@
+package com.example.latihanandroid.datamodels
+
+data class InternshipItem(
+    val endAt: String? = null,
+    val agency: String? = null,
+    val supervisorId: Int? = null,
+    val grade: String? = null,
+    val id: Int? = null,
+    val title: String? = null,
+    val startAt: Any? = null,
+    val supervisor: String? = null,
+    val status: Int? = null
+)
---
+++ app/src/main/java/com/example/latihanandroid/datamodels/ListkpResponse.kt
@@ -0,0 +1,7 @@
+package com.example.latihanandroid.datamodels
+
+data class ListkpResponse(
+    val count: Int? = null,
+    val status: String? = null,
+    val internship: List<InternshipItem?>? = null
+)
